4a8953470fd879839837cdff3d6c63ca0b993621
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-06-18 23:02:13 +0530

    Register Form Added Successfully!

--- Servlet/RegiterAndLoginWithDB/src/main/java/com/qsp/Register.java
+++ Servlet/RegiterAndLoginWithDB/src/main/java/com/qsp/Register.java
@@ -1,50 +1,135 @@
 package com.qsp;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
 
+import java.sql.*;
+
 import javax.servlet.GenericServlet;
+
 import javax.servlet.ServletException;
+
 import javax.servlet.ServletRequest;
+
 import javax.servlet.ServletResponse;
+
 import javax.servlet.annotation.WebServlet;
 
-//==>> By using this anotation we can map the html page with 
-@WebServlet("/register") 
-public class Register extends GenericServlet{
+@WebServlet("/register")
 
-	@Override
-	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+public class Register extends GenericServlet {
+
+@Override
+
+public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+
+
+	//Read All Data
+
+	String id = req.getParameter("id");
+
+	int id1 = Integer.parseInt(id);
+
+	
+
+	String name =  req.getParameter("name");
+
+	
+
+	String mobno = req.getParameter("mobno");
+
+	long mobno1 = Long.parseLong(mobno);
+
+	
+
+	String email = req.getParameter("email");
+
+	
+
+	String username = req.getParameter("username");
+
+	String password = req.getParameter("pass");
+
+	
+
+	
+
+    Connection connection = null;
+
+    
+
+    try {
+
+		Class.forName("org.postgresql.Driver");
+
 		
-		// Read all the data from frontend.
+
+		connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ServletRegister?user=postgres&password=root");
+
 		
-		String id = req.getParameter("id");
+
+		PreparedStatement statement = connection.prepareStatement("insert into customer values(?,?,?,?,?,?)");
+
+		statement.setInt(1, id1);
+
+		statement.setString(2, name);
+
+		statement.setLong(3, mobno1);
+
+		statement.setString(4, email);
+
+		statement.setString(5, username);
+
+		statement.setString(6, password);
+
 		
-		int id1 = Integer.parseInt(id);
+
+		statement.execute();
+
 		
-		String name = req.getParameter("name");
-		
-		String phone = req.getParameter("phone");
-		
-		long phone1 = Long.parseLong(phone);
-		
-		String email = req.getParameter("email");
-		
-		String username = req.getParameter("username");
-		
-		String password = req.getParameter("password");
-		
-		System.out.println("Register.java file executed...");
-		
+
 		PrintWriter writer = res.getWriter();
+
+		 writer.print("<html><body><h2>Registration Successful...</h2></body></html>");
+		 writer.print("<html><body><h1>"+id+"</h1></body></html>");
+		 writer.print("<html><body><h1>"+name+"</h1></body></html>");
+		 writer.print("<html><body><h1>"+mobno+"</h1></body></html>");
+		 writer.print("<html><body><h1>"+email+"</h1></body></html>");
+		 writer.print("<html><body><h1>"+username+"</h1></body></html>");
+		 writer.print("<html><body><h1>"+password+"</h1></body></html>");
+
 		
-		writer.print("<html>"
-				+ "<body>"
-				+ "<h1>Registration Successfully.....!</h1>"
-				+	id1 + "<br>" + name + "<br>" + phone1 + "<br>" + email + "<br>" + username + "<br>" + password + "<br>"
-				+ "</body>"
-				+ "</html>");
+
+	} catch (Exception e) {
+
+		// TODO Auto-generated catch block
+
+		e.printStackTrace();
+
 	}
+
+    finally {
+
+    	try {
+
+			connection.close();
+
+		} catch (SQLException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+    }
+
+    
+
+	
+
+}
 
 }
