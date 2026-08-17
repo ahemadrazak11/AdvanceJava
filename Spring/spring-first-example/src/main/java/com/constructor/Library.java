3376b5494efa69db1d3dace91c2c2a26dab47dea
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-08-17 21:46:34 +0530

    Spring Project Added.

---
+++ Spring/spring-first-example/src/main/java/com/constructor/Library.java
@@ -0,0 +1,14 @@
+package com.constructor;
+
+public class Library {
+
+	int id;
+	String name;
+	String location;
+	
+	Library(int id, String name, String location){
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+}
---
+++ Spring/spring-first-example/src/main/java/com/constructor/LibraryDriver.java
@@ -0,0 +1,17 @@
+package com.constructor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryDriver {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("LibraryConfig.xml");
+		
+		Library l = (Library) applicationContext.getBean("Library");
+		
+		System.out.println(l.id);
+		System.out.println(l.name);
+		System.out.println(l.location);
+	}
+}
